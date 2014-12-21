5682914f1321bb338aa5ee080c8c62c4cc11101a
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-12-21 07:27:04 -0700

    clock: update package documentation; See #71

--- clock/doc.go
+++ clock/doc.go
@@ -4,17 +4,9 @@
 
 // Package clock measures frame-based application performance.
 //
-// A Time function is provided, which returns the time since the program
-// started, it can be very useful on it's own, as on Windows this package uses
-// cgo to call QueryPerformanceFrequency and QueryPerformanceCounter, which
-// provides a higher resolution time than Go's time package.
+// Typically you create a single Clock object for each "renderer" and invoke
+// Tick on it at the start of each frame.
 //
-// An the case of an visual application, an typical use case would be creating
-// an single Clock for each "renderer", and then invoke Clock.Tick() at the
-// start of every frame before it is rendered.
-//
-// When using a maximum frame rate, Clock.Tick blocks for an little while to
-// ensure running at least under Clock.MaxFrameRate(), if you simply ignore
-// this blocking or push it to another goroutine, you'll lose the whole point
-// of having an maximum frame rate specified.
+// When using a maximum frame rate, Tick blocks just long enough to ensure that
+// the application is at max running at MaxFrameRate.
 package clock
