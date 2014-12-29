209af4f92de740d759ce8d2fcbcb585c64cc4fa1
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-12-28 19:57:18 -0700

    internal/tag: add graphics state guard debug tag (gsgdebug).

---
+++ internal/tag/gsgdebug.go
@@ -0,0 +1,8 @@
+// Copyright 2014 The Azul3D Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// +build gsgdebug
+
+package tag
+
+const Gsgdebug = true
