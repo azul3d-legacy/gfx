67f9c2d6138813263caa2b9eb4849ebfd0cd1d10
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-01 23:00:42 -0700

    internal/glc: implement many more common functionality

--- internal/glc/gles2context.go
+++ internal/glc/gles2context.go
@@ -5,12 +5,34 @@
 
 package glc
 
-import gl "azul3d.org/gfx.v2-dev/internal/gles2/2.0/gles2"
+import (
+	"azul3d.org/gfx.v2-dev"
+	gl "azul3d.org/gfx.v2-dev/internal/gles2/2.0/gles2"
+)
 
 type glFuncs struct {
-	GetError func() int
-	Enable   func(capability int)
-	Disable  func(capability int)
+	GetError              func() int
+	Enable                func(capability int)
+	Disable               func(capability int)
+	Scissor               func(x, y, width, height int)
+	ColorMask             func(r, g, b, a bool)
+	ClearColor            func(r, g, b, a float32)
+	ClearDepth            func(depth float64)
+	ClearStencil          func(s int)
+	DepthMask             func(b bool)
+	DepthFunc             func(f int)
+	CullFace              func(m int)
+	BlendColor            func(r, g, b, a float32)
+	BlendFuncSeparate     func(srcRGB, dstRGB, srcAlpha, dstAlpha int)
+	BlendEquationSeparate func(modeRGB, modeAlpha int)
+	StencilOpSeparate     func(face, fail, zfail, zpass int)
+
+	GetScissorBox       func() (x, y, width, height int)
+	GetColorWriteMask   func() (r, g, b, a bool)
+	GetParameterColor   func(p int) gfx.Color
+	GetParameterBool    func(p int) bool
+	GetParameterInt     func(p int) int
+	GetParameterFloat64 func(p int) float64
 }
 
 type Context struct {
@@ -67,13 +89,90 @@ type Context struct {
 	FUNC_ADD              int
 	FUNC_SUBTRACT         int
 	FUNC_REVERSE_SUBTRACT int
+
+	DITHER                   int
+	SCISSOR_TEST             int
+	STENCIL_TEST             int
+	DEPTH_TEST               int
+	CULL_FACE                int
+	BLEND                    int
+	SAMPLE_ALPHA_TO_COVERAGE int
+	MULTISAMPLE              int
+
+	DEPTH_WRITEMASK              int
+	COLOR_CLEAR_VALUE            int
+	BLEND_COLOR                  int
+	DEPTH_CLEAR_VALUE            int
+	STENCIL_CLEAR_VALUE          int
+	DEPTH_FUNC                   int
+	CULL_FACE_MODE               int
+	BLEND_SRC_RGB                int
+	BLEND_DST_RGB                int
+	BLEND_SRC_ALPHA              int
+	BLEND_DST_ALPHA              int
+	BLEND_EQUATION_RGB           int
+	BLEND_EQUATION_ALPHA         int
+	STENCIL_FAIL                 int
+	STENCIL_PASS_DEPTH_FAIL      int
+	STENCIL_PASS_DEPTH_PASS      int
+	STENCIL_BACK_FAIL            int
+	STENCIL_BACK_PASS_DEPTH_FAIL int
+	STENCIL_BACK_PASS_DEPTH_PASS int
 }
 
 func NewContext() *Context {
 	f := &glFuncs{
-		GetError: func() int { return int(gl.GetError()) },
-		Enable:   func(capability int) { gl.Enable(uint32(capability)) },
-		Disable:  func(capability int) { gl.Disable(uint32(capability)) },
+		GetError:     func() int { return int(gl.GetError()) },
+		Enable:       func(capability int) { gl.Enable(uint32(capability)) },
+		Disable:      func(capability int) { gl.Disable(uint32(capability)) },
+		Scissor:      func(x, y, width, height int) { gl.Scissor(int32(x), int32(y), int32(width), int32(height)) },
+		ColorMask:    gl.ColorMask,
+		ClearColor:   gl.ClearColor,
+		ClearDepth:   func(depth float64) { gl.ClearDepthf(float32(depth)) },
+		ClearStencil: func(stencil int) { gl.ClearStencil(int32(stencil)) },
+		DepthMask:    gl.DepthMask,
+		DepthFunc:    func(f int) { gl.DepthFunc(uint32(f)) },
+		CullFace:     func(m int) { gl.CullFace(uint32(m)) },
+		BlendColor:   gl.BlendColor,
+		BlendFuncSeparate: func(srcRGB, dstRGB, srcAlpha, dstAlpha int) {
+			gl.BlendFuncSeparate(uint32(srcRGB), uint32(dstRGB), uint32(srcAlpha), uint32(dstAlpha))
+		},
+		BlendEquationSeparate: func(modeRGB, modeAlpha int) {
+			gl.BlendEquationSeparate(uint32(modeRGB), uint32(modeAlpha))
+		},
+		StencilOpSeparate: func(face, fail, zfail, zpass int) {
+			gl.StencilOpSeparate(uint32(face), uint32(fail), uint32(zfail), uint32(zpass))
+		},
+		GetScissorBox: func() (x, y, width, height int) {
+			var s [4]int32
+			gl.GetIntegerv(gl.SCISSOR_BOX, &s[0])
+			return int(s[0]), int(s[1]), int(s[2]), int(s[3])
+		},
+		GetColorWriteMask: func() (r, g, b, a bool) {
+			var s [4]bool
+			gl.GetBooleanv(gl.COLOR_WRITEMASK, &s[0])
+			return s[0], s[1], s[2], s[3]
+		},
+		GetParameterColor: func(p int) gfx.Color {
+			var c gfx.Color
+			gl.GetFloatv(uint32(p), &c.R)
+			return c
+		},
+		GetParameterBool: func(p int) bool {
+			var b bool
+			gl.GetBooleanv(uint32(p), &b)
+			return b
+		},
+		GetParameterInt: func(p int) int {
+			var i int32
+			gl.GetIntegerv(uint32(p), &i)
+			return int(i)
+		},
+		GetParameterFloat64: func(p int) float64 {
+			var f float32
+			gl.GetFloatv(uint32(p), &f)
+			return float64(f)
+		},
 	}
 
 	return &Context{
@@ -130,5 +229,41 @@ func NewContext() *Context {
 		FUNC_ADD:              gl.FUNC_ADD,
 		FUNC_SUBTRACT:         gl.FUNC_SUBTRACT,
 		FUNC_REVERSE_SUBTRACT: gl.FUNC_REVERSE_SUBTRACT,
+
+		DITHER:       gl.DITHER,
+		SCISSOR_TEST: gl.SCISSOR_TEST,
+		STENCIL_TEST: gl.STENCIL_TEST,
+		DEPTH_TEST:   gl.DEPTH_TEST,
+		CULL_FACE:    gl.CULL_FACE,
+		BLEND:        gl.BLEND,
+		SAMPLE_ALPHA_TO_COVERAGE: gl.SAMPLE_ALPHA_TO_COVERAGE,
+
+		// TODO(slimsag): Find out if this is valid OpenGL ES 2.0 ?
+		//
+		// OpenGL ES 2.0 spec makes various references to the GL_MULTISAMPLE
+		// constant, but it's not defined anywhere. Apparently multisampling
+		// is enabled by default (i.e. multisample is always on if you request
+		// a multisample EGL framebuffer).
+		MULTISAMPLE: 0x809D,
+
+		DEPTH_WRITEMASK:              gl.DEPTH_WRITEMASK,
+		COLOR_CLEAR_VALUE:            gl.COLOR_CLEAR_VALUE,
+		BLEND_COLOR:                  gl.BLEND_COLOR,
+		DEPTH_CLEAR_VALUE:            gl.DEPTH_CLEAR_VALUE,
+		STENCIL_CLEAR_VALUE:          gl.STENCIL_CLEAR_VALUE,
+		DEPTH_FUNC:                   gl.DEPTH_FUNC,
+		CULL_FACE_MODE:               gl.CULL_FACE_MODE,
+		BLEND_SRC_RGB:                gl.BLEND_SRC_RGB,
+		BLEND_DST_RGB:                gl.BLEND_DST_RGB,
+		BLEND_SRC_ALPHA:              gl.BLEND_SRC_ALPHA,
+		BLEND_DST_ALPHA:              gl.BLEND_DST_ALPHA,
+		BLEND_EQUATION_RGB:           gl.BLEND_EQUATION_RGB,
+		BLEND_EQUATION_ALPHA:         gl.BLEND_EQUATION_ALPHA,
+		STENCIL_FAIL:                 gl.STENCIL_FAIL,
+		STENCIL_PASS_DEPTH_FAIL:      gl.STENCIL_PASS_DEPTH_FAIL,
+		STENCIL_PASS_DEPTH_PASS:      gl.STENCIL_PASS_DEPTH_PASS,
+		STENCIL_BACK_FAIL:            gl.STENCIL_BACK_FAIL,
+		STENCIL_BACK_PASS_DEPTH_FAIL: gl.STENCIL_BACK_PASS_DEPTH_FAIL,
+		STENCIL_BACK_PASS_DEPTH_PASS: gl.STENCIL_BACK_PASS_DEPTH_PASS,
 	}
 }
--- internal/glc/jscontext.go
+++ internal/glc/jscontext.go
@@ -5,10 +5,26 @@
 
 package glc
 
-import "github.com/gopherjs/webgl"
+import (
+	"azul3d.org/gfx.v2-dev"
+	"github.com/gopherjs/webgl"
+)
+
+type glFuncs struct {
+	*webgl.Context
+	BlendColor        func(r, g, b, a float32)
+	StencilOpSeparate func(face, fail, zfail, zpass int)
+
+	GetScissorBox       func() (x, y, width, height int)
+	GetColorWriteMask   func() (r, g, b, a bool)
+	GetParameterColor   func(p int) gfx.Color
+	GetParameterBool    func(p int) bool
+	GetParameterInt     func(p int) int
+	GetParameterFloat64 func(p int) float64
+}
 
 type Context struct {
-	gl *webgl.Context
+	gl *glFuncs
 	*webgl.Context
 
 	// TODO(slimsag): add to webgl bindings.
@@ -17,13 +33,52 @@ type Context struct {
 	// WebGL doesn't have these errors, they are faked here for GetError.
 	STACK_OVERFLOW  int
 	STACK_UNDERFLOW int
+
+	MULTISAMPLE int
 }
 
 func NewContext(ctx *webgl.Context) *Context {
 	return &Context{
-		gl:      ctx,
+		gl: &glFuncs{
+			Context: ctx,
+			BlendColor: func(r, g, b, a float32) {
+				ctx.BlendColor(float64(r), float64(g), float64(b), float64(a))
+			},
+			StencilOpSeparate: func(face, fail, zfail, zpass int) {
+				// TODO(slimsag): add to webgl bindings.
+				ctx.Call("stencilOpSeparate", face, fail, zfail, zpass)
+			},
+			GetScissorBox: func() (x, y, width, height int) {
+				sb := ctx.GetParameter(ctx.SCISSOR_BOX).Interface().([]int)
+				return sb[0], sb[1], sb[2], sb[3]
+			},
+			GetColorWriteMask: func() (r, g, b, a bool) {
+				cwm := ctx.GetParameter(ctx.COLOR_WRITEMASK)
+				r = cwm.Index(0).Bool()
+				g = cwm.Index(1).Bool()
+				b = cwm.Index(2).Bool()
+				a = cwm.Index(3).Bool()
+				return
+			},
+			GetParameterColor: func(p int) gfx.Color {
+				f := ctx.GetParameter(p).Interface().([]float32)
+				return gfx.Color{R: f[0], G: f[1], B: f[2], A: f[3]}
+			},
+			GetParameterBool: func(p int) bool {
+				return ctx.GetParameter(p).Bool()
+			},
+			GetParameterInt: func(p int) int {
+				return ctx.GetParameter(p).Int()
+			},
+			GetParameterFloat64: func(p int) float64 {
+				return ctx.GetParameter(p).Float()
+			},
+		},
 		Context: ctx,
 		ALWAYS:  519,
+
+		// TODO(slimsag): Find out if this is valid WebGL ? See gles2context.go
+		MULTISAMPLE: 0x809D,
 
 		STACK_OVERFLOW:  -1024,
 		STACK_UNDERFLOW: -1025,
