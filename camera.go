bf7e5ba413f43fa9e97227edbba065888555a839
Olav Andreas Lindekleiv <olav@lindekleiv.com>
2015-01-15 14:50:42 +0100

    Add .FOV(), .Near() and .Far() to camera. azul3d/gfx#91

--- camera.go
+++ camera.go
@@ -29,6 +29,8 @@ type Camera struct {
 	// The projection matrix of the camera, which is responsible for projecting
 	// world coordinates into device coordinates.
 	Projection Mat4
+
+	fov, near, far float64
 }
 
 // SetOrtho sets this camera's Projection matrix to an orthographic one.
@@ -49,6 +51,10 @@ func (c *Camera) SetOrtho(view image.Rectangle, near, far float64) {
 	h = float64(int((h / 2.0)) * 2)
 	m := lmath.Mat4Ortho(0, w, 0, h, near, far)
 	c.Projection = ConvertMat4(m)
+
+	c.fov = -1
+	c.near = near
+	c.far = far
 }
 
 // SetPersp sets this camera's Projection matrix to an perspective one.
@@ -69,6 +75,10 @@ func (c *Camera) SetPersp(view image.Rectangle, fov, near, far float64) {
 	aspectRatio := float64(view.Dx()) / float64(view.Dy())
 	m := lmath.Mat4Perspective(fov, aspectRatio, near, far)
 	c.Projection = ConvertMat4(m)
+
+	c.fov = fov
+	c.near = near
+	c.far = far
 }
 
 // Project returns a 2D point in normalized device space coordinates given a 3D
@@ -87,6 +97,22 @@ func (c *Camera) Project(p3 lmath.Vec3) (p2 lmath.Vec2, ok bool) {
 	return
 }
 
+// FOV returns the field of view of the camera. For an orthographic camera, this
+// will always be -1.
+func (c *Camera) FOV() float64 {
+	return c.fov
+}
+
+// Near returns the minimum closest clipping point of the view frustum.
+func (c *Camera) Near() float64 {
+	return c.near
+}
+
+// Near returns the maximum furthest clipping point of the view frustum.
+func (c *Camera) Far() float64 {
+	return c.far
+}
+
 // Copy returns a new copy of this Camera.
 func (c *Camera) Copy() *Camera {
 	return &Camera{
@@ -99,6 +125,9 @@ func (c *Camera) Copy() *Camera {
 func (c *Camera) Reset() {
 	c.Object.Reset()
 	c.Projection = ConvertMat4(lmath.Mat4Identity)
+	c.fov = 0.0
+	c.near = 0.0
+	c.far = 0.0
 }
 
 // Destroy destroys this camera for use by other callees to NewCamera. You must
@@ -115,6 +144,7 @@ var camPool = sync.Pool{
 		return &Camera{
 			NewObject(),
 			ConvertMat4(lmath.Mat4Identity),
+			0.0, 0.0, 0.0,
 		}
 	},
 }
