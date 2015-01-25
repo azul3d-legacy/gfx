c9172c62825ad49d27d5fbaf20712bfbabd9c818
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-25 02:37:26 -0700

    camera: fix test

--- camera/camera_test.go
+++ camera/camera_test.go
@@ -4,7 +4,11 @@
 
 package camera
 
-import "azul3d.org/gfx.v2-unstable"
+import (
+	"image"
+
+	"azul3d.org/gfx.v2-unstable"
+)
 
 // A check for whether or not *camera.Camera implements gfx.Camera properly.
-var _ gfx.Camera = New()
+var _ gfx.Camera = New(image.Rectangle{})
