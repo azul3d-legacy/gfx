b6873a40f54305cdc2b24ce9804fdf9ee8f84c71
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-25 02:34:40 -0700

    camera: add missing license header

--- camera/camera_test.go
+++ camera/camera_test.go
@@ -1,3 +1,7 @@
+// Copyright 2014 The Azul3D Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package camera
 
 import "azul3d.org/gfx.v2-unstable"
